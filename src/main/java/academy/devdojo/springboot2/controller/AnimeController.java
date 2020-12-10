88457e3542a1c7e6087b878bf5871cfd6273a020
OruamC <oru_catani@hotmail.com>
2020-12-09 23:30:30 -0300

    Finishing CRUD

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -5,12 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.service.AnimeService;
@@ -37,9 +32,21 @@ public class AnimeController {
 	public ResponseEntity<Anime> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(animeService.findById(id));
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Anime> save(@RequestBody Anime anime) {
 		return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		animeService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@PutMapping
+	public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+		animeService.replace(anime);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
