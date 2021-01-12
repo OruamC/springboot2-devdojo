fc1c15611c9d2bcb680f6c1c38dc289680024206
OruamC <oru_catani@hotmail.com>
2021-01-12 14:22:55 -0300

    Authentication with user from BD

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package academy.devdojo.springboot2.config;
 
+import academy.devdojo.springboot2.service.DevDojoUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +12,13 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Log4j2
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+@SuppressWarnings("java:S5344")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DevDojoUserDetailsService devDojoUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,13 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder password = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", password.encode("curso"));
         auth.inMemoryAuthentication()
-                .withUser("oruam")
+                .withUser("oruam2")
                 .password(password.encode("curso"))
                 .roles("USER", "ADMIN")
                 .and()
-                .withUser("devdojo")
+                .withUser("devdojo2")
                 .password(password.encode("curso"))
                 .roles("USER");
+
+        auth.userDetailsService(devDojoUserDetailsService)
+                .passwordEncoder(password);
     }
 }
