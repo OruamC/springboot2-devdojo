12a24405efe4e9142c9336914faf482dd6d8c738
OruamC <oru_catani@hotmail.com>
2021-01-13 15:42:37 -0300

    URL protection with AntMatcher

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,10 +1,11 @@
 package academy.devdojo.springboot2.controller;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
+import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
+import academy.devdojo.springboot2.service.AnimeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -14,13 +15,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import academy.devdojo.springboot2.domain.Anime;
-import academy.devdojo.springboot2.service.AnimeService;
-import academy.devdojo.springboot2.util.DateUtil;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("animes")
@@ -46,7 +42,6 @@ public class AnimeController {
 	}
 
 	@GetMapping(path = "by-id/{id}")
-	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable Long id,
 																 @AuthenticationPrincipal UserDetails userDetails) {
 		log.info(userDetails);
@@ -59,12 +54,11 @@ public class AnimeController {
 	}
 
 	@PostMapping
-	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
 		return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
 	}
-	
-	@DeleteMapping(path = "/{id}")
+
+	@DeleteMapping(path = "/admin/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		animeService.delete(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
